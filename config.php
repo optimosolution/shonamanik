e97bfd4afc47bb4e0679650446a5f6a2275f8dc1
Rana <saidurwd@gmail.com>
2017-02-27 22:10:02 +0600

    First upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8888/shonamanik/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8888/shonamanik/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/shonamanik/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/shonamanik/system/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/shonamanik/image/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/shonamanik/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/shonamanik/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/shonamanik/system/config/');
+define('DIR_CACHE', 'D:/xampp/htdocs/shonamanik/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/shonamanik/system/storage/download/');
+define('DIR_LOGS', 'D:/xampp/htdocs/shonamanik/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/shonamanik/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/shonamanik/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shonamanik');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'sm_');
